d42701662a019dcbb9519db0710f03706d71ac15
Kannan Ramakrishnan <12591116+kannanramakrishnan@users.noreply.github.com>
2024-05-04 20:50:23 +0100

    Update ReverseWords.java

--- ReverseWords.java
+++ ReverseWords.java
@@ -1,22 +1,10 @@
 class ReverseWords {
     public String reverseWords(String s) {
-        String[] words = s.split("\\s+");
-
-        ArrayList<String> wordsList = new ArrayList<>(Arrays.asList(words));
-        List<String> reverseWordsList = new ArrayList<>();
-
-        for(int i = wordsList.size()-1; i>=0; i--)
-        {
-            reverseWordsList.add(wordsList.get(i));
-        }
-
-        StringBuilder sb = new StringBuilder();
-
-        for(String word: reverseWordsList)
-        {
-            sb.append(word).append(" ");
-        }
-
-        return sb.toString().trim();
+     String[] ws = s.trim().split("\\s+");
+     StringBuilder st = new StringBuilder();
+     for(int i= ws.length -1; i>=0;i--){
+        st.append(ws[i]).append(" ");
+     }
+     return st.toString().trim();
     }
 }
