7998ce074246980f99d4a81a61bb686dfbfb5b8c
Kannan Ramakrishnan <12591116+kannanramakrishnan@users.noreply.github.com>
2024-05-05 21:37:31 +0100

    Create MaxNumbersOfKSumPairs.java

---
+++ MaxNumbersOfKSumPairs.java
@@ -0,0 +1,25 @@
+class MaxNumbersOfKSumPairs {
+    public int maxOperations(int[] nums, int k) {
+        Arrays.sort(nums);
+        int left = 0;
+        int right = nums.length-1;
+        int answer = 0;
+
+        while(left < right)
+        {
+            int sum = nums[left]+nums[right];
+
+            if(sum == k)
+            {
+                ++answer;
+                left++;
+                right--;
+            } else if (sum > k) {
+                right --;
+            } else {
+                left++;
+            }
+        }
+        return answer;
+    }
+}
