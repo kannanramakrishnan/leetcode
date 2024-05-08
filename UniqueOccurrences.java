a2131d60ab00090ae8dd498542d5f97760fc4f4f
Kannan Ramakrishnan <12591116+kannanramakrishnan@users.noreply.github.com>
2024-05-08 22:07:30 +0100

    Create UniqueOccurrences.java

---
+++ UniqueOccurrences.java
@@ -0,0 +1,16 @@
+class UniqueOccurrences {
+    public boolean uniqueOccurrences(int[] arr) {
+        Map<Integer, Integer> freq = new HashMap<>();
+        Set<Integer> s = new HashSet<>();
+
+        for(int i:arr) {
+            freq.put(i, freq.getOrDefault(i, 0)+1);
+        }
+
+        for(int x: freq.values()) {
+            s.add(x);
+        }
+
+        return s.size() == freq.size();
+    }
+}
