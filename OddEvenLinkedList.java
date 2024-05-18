e9b3f7494a92c14798931b915844a2267eb56faa
Kannan Ramakrishnan <12591116+kannanramakrishnan@users.noreply.github.com>
2024-05-18 19:49:51 +0100

    Create OddEvenLinkedList.java

---
+++ OddEvenLinkedList.java
@@ -0,0 +1,24 @@
+class OddEvenLinkedList {
+    public ListNode oddEvenList(ListNode head) {
+        if(head == null||head.next == null) {
+            return head;
+        }
+
+        ListNode odd = head;
+        ListNode oddHead = odd;
+        ListNode even = head.next;
+        ListNode evenHead = even;
+
+        while(even != null && even.next != null)
+        {
+            odd.next = even.next;
+            odd = odd.next;
+            even.next = odd.next;
+            even = even.next;
+        }
+
+        odd.next = evenHead;
+
+        return oddHead;
+    }
+}
