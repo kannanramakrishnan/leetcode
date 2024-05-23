91d3c60a928d54152a8c7caa77ba0b0626fb332b
Kannan Ramakrishnan <12591116+kannanramakrishnan@users.noreply.github.com>
2024-05-23 19:59:38 +0100

    Update MaximumSubsequenceScore.java

--- MaximumSubsequenceScore.java
+++ MaximumSubsequenceScore.java
@@ -1,18 +1,24 @@
 class MaximumSubsequenceScore {
-    record Pair(int a, int b) {}
-    public long maxScore(int[] a, int[] b, int k) {
-        int n = a.length;
-        Pair[] pairs = new Pair[n];
-        for (int i = 0; i < n; ++i) pairs[i] = new Pair(a[i], b[i]);
-        Arrays.sort(pairs, (x, y) -> y.b - x.b);
-        Queue<Integer> q = new PriorityQueue<>(k+1);
-        long res = 0, sum = 0;
-        for (var p : pairs) {
-            q.add(p.a);
-            sum += p.a;
-            if (q.size() > k) sum -= q.poll();
-            if (q.size() == k) res = Math.max(res, sum * p.b);
+    public long maxScore(int[] nums1, int[] nums2, int k) {
+        int n = nums1.length;
+        int[][] res = new int[n][2];
+        for(int i=0; i<n; i++){
+            res[i] = new int[]{nums1[i], nums2[i]};
         }
-        return res;
+
+        Arrays.sort(res, (a, b) -> b[1] - a[1]);
+
+        long ans=0;
+        long sum=0;
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        for(int i=0; i<n; i++){
+            sum += res[i][0];
+            pq.offer(res[i][0]);
+            if(pq.size() == k){
+                ans = Math.max(ans, sum*res[i][1]);
+                sum -= pq.poll();
+            }
+        }
+        return ans;
     }
 }
