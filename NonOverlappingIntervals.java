c84b6655755026aca9ab57a3efd81a5a7ad285b2
Kannan Ramakrishnan <12591116+kannanramakrishnan@users.noreply.github.com>
2024-05-25 19:46:52 +0100

    Create NonOverlappingIntervals.java

---
+++ NonOverlappingIntervals.java
@@ -0,0 +1,21 @@
+class NonOverlappingIntervals {
+    public int eraseOverlapIntervals(int[][] intervals) {
+        Arrays.sort(intervals, new Comparator<int[]>() {
+            public int compare(int[] a, int[] b) {
+                return a[1] - b[1];
+            }
+        });
+
+        int remove = 0;
+        int end = -100000;
+        for (int[] interval : intervals) {
+            if (interval[0] >= end) {
+                end = interval[1];
+            } else {
+                remove++;
+            }
+        }
+
+        return remove;
+    }
+}
